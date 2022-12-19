79123b870decb69d21e74ea1c27e5205f361b6ee
akash1700 <88049828+akash1700@users.noreply.github.com>
2022-12-19 16:51:39 +0530

    Add files via upload

---
+++ CMakeProject1.cpp
@@ -0,0 +1,13 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1.h"
+#include "iostream"
+using namespace std;
+
+int main()
+{
+	string str = "Hello World from Cmake";
+	cout <<str<< endl;
+	return 0;
+}
